c896d00f20813b97fdc955120183a722ab39ac21
slcc2c <slcc2c@icloud.com>
2015-11-17 13:43:33 -0600

    Added median class/header

---
+++ Chapter 1/Chapter 4/Chapter 4.cpp
@@ -0,0 +1,13 @@
+// Chapter 4.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <algorithm>
+#include <iomanip>
+#include <stdexcept>
+
+int main()
+{
+    return 0;
+}
+
